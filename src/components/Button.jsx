d60841f65905727f8c48a2017c87f8e61696e67b
nue-os <kimsoeun0108@gmail.com>
2025-04-25 23:00:48 +0900

    style: 버튼 색상 동적 적용

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,11 @@ import css from './Button.module.css'
 
 const Button = ({ color = 'primary', text, onClick }) => {
   const buttonClass = `${css.button} ${css[color]}`
-  return <button onClick={buttonClass}>{text}</button>
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {text}
+    </button>
+  )
 }
 
 export default Button
