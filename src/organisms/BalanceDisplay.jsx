7bafaac65d97f7d0278658dc0e1915a17d147c21
nue-os <kimsoeun0108@gmail.com>
2025-04-25 23:18:24 +0900

    feat: 잔액 데이터 바인딩

--- src/organisms/BalanceDisplay.jsx
+++ src/organisms/BalanceDisplay.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import css from './BalanceDisplay.module.css'
+import { useSelector } from 'react-redux'
+import { calBalance } from '../utils/calculate'
 
 const BalanceDisplay = () => {
+  const { transactions } = useSelector(state => state.transactions)
+
   return (
     <div className={css.balanceCon}>
       <strong>잔액</strong>
-      <span>{`￦200,000`}</span>
+      <span>{`￦${Number(calBalance(transactions)).toLocaleString()}`}</span>
     </div>
   )
 }
